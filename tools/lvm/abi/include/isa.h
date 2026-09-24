b6f16b301378709bc21a3cc088bd72956121eeb3
Sayan Malik <sayan.malik.102924@gmail.com>
2026-09-24 22:36:34 +0530

    Refactor opcode definitions: rename OP_PUSHR and OP_POPR for consistency; adjust opcode values for clarity

--- tools/lvm/abi/include/isa.h
+++ tools/lvm/abi/include/isa.h
@@ -87,13 +87,23 @@ typedef enum : uint8_t {
 	
 	OP_CALL      = 0x36,  // Call Subroutine
 	OP_RET       = 0x37,  // Return from Subroutine
-	OP_PUSHR	 = 0x38,
-	OP_POPR		 = 0x39,
+	OP_PUSH		 = 0x38,
+	OP_POP		 = 0x39,
+	OP_PUSHR	 = 0x3A,
+	OP_POPR		 = 0x3B,
 	
-	OP_CNV       = 0x3A,  // Type Conversion / Cast
+	OP_CNV       = 0x3C,  // Type Conversion / Cast
 } opcode_t;
  
+#define OPCODE_RANGE_1 63	// 0 - 63
+#define OPCODE_RANGE_2 95	// 0 - 95
+#define OPCODE_RANGE_3 127  // 0 - 127
+#define OPCODE_RANGE_4 159  // 0 - 159
+#define OPCODE_RANGE_5 191	// 0 - 191
+#define OPCODE_RANGE_6 223	// 0 - 223
+#define OPCODE_RANGE_max 255 	// 0 - 255
 
 
+#define CURRENT_OPCODE_RANGE OPCODE_RANGE_1
 
 #endif /* ISA_H */
