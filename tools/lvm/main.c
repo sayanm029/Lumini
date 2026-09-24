2ab38af6637177346fdcd7d8de64eb36dfaa9ab5
Sayan Malik <sayan.malik.102924@gmail.com>
2026-09-25 01:42:59 +0530

    Fix CLI bug

--- tools/lvm/main.c
+++ tools/lvm/main.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdbool.h>
 
 
 int main(int argc, char** argv)
@@ -11,8 +14,8 @@ int main(int argc, char** argv)
 	 * Argument Validation
 	 * ===================================================================== */
 	if (argc < 2) {
-		fprintf(stdout, "error: no files provided\n");
-		return 0;
+		fprintf(stderr, "error: no files provided\n");
+		return 1;
 	}
 
 
@@ -27,16 +30,17 @@ int main(int argc, char** argv)
 			"Usage: lvm <program path> [option]\n\n"
 
 			"Options:\n"
-			"lvm  --help       			Show this help message\n"
-			"lvm  --version    			Print version information\n"
-			"lvm  -v           			Print detailed VM specification\n"
-			"lvm <program path>  -e     Print the VM return code after execution\n"
-			"lvm <program path>  -es    Print the VM return code with its description\n"
-			"lvm  -e=<code>    			Explain a specific return code only. No execution.\n"
+			"lvm  --help                 Show this help message\n"
+			"lvm  --version              Print version information\n"
+			"lvm  -v                     Print detailed VM specification\n"
+			"lvm <program path> -e      Print the VM return code after execution\n"
+			"lvm <program path> -es     Print the VM return code with its description\n"
+			"lvm  -e=<code>              Explain a specific return code only. No execution.\n"
 		);
 
 		return 0;
 	}
+
 
 	/* Version */
 	if (strcmp(argv[1], "--version") == 0) {
@@ -44,19 +48,23 @@ int main(int argc, char** argv)
 			LVM_VERSION_MAJOR,
 			LVM_VERSION_MINOR,
 			SYSTEM_ARCH);
+
 		return 0;
 	}
 
+
 	/* Detailed Specification */
 	if (strcmp(argv[1], "-v") == 0) {
-		fprintf(stdout, "lvm v%d.%d.0 (%s)\n"
-		                "installed path: %s\n",
+		fprintf(stdout,
+			"lvm v%d.%d.0 (%s)\n"
+			"installed path: %s\n",
 			LVM_VERSION_MAJOR,
 			LVM_VERSION_MINOR,
 			SYSTEM_ARCH,
 			argv[0]);
 
 		fprintf(stdout, "abi version: %d.0\n", ABI_VERSION);
+
 		return 0;
 	}
 
@@ -64,7 +72,7 @@ int main(int argc, char** argv)
 	/* =====================================================================
 	 * Variables & Flags
 	 * ===================================================================== */
-	int explain_code;
+	int explain_code = 0;
 
 	bool flag_explain = false;
 	bool show_string = false;
@@ -72,67 +80,84 @@ int main(int argc, char** argv)
 
 
 	/* =====================================================================
-	 * Command-Line Flag Parsing
+	 * Standalone Return-Code Explanation
 	 * ===================================================================== */
 
-	/* Explain return code directly: -e=<code> */
-	if (argc >= 2) {
-		if (strncmp(argv[1], "-e=", 3) == 0) {
-			char *endptr;
+	/*
+	 * Syntax:
+	 *
+	 *     lvm -e=<code>
+	 *
+	 * No VM execution occurs in this mode.
+	 */
+	if (strncmp(argv[1], "-e=", 3) == 0) {
+		char *endptr;
+		const char *value = argv[1] + 3;
 
-			errno = 0;
+		errno = 0;
 
-			long val = strtol(argv[2] + 3, &endptr, 10);
+		long val = strtol(value, &endptr, 10);
 
-			/* Validate parsed value */
-			if (endptr == argv[2] + 3) {
-				fprintf(stderr,
-					"Error: No digits found after -e=\n");
-
-			} else if (*endptr != '\0') {
-				fprintf(stderr,
-					"Error: Trailing garbage characters found: '%s'\n",
-					endptr);
-
-			} else if ((val == LONG_MIN || val == LONG_MAX)
-				   && errno == ERANGE) {
-				fprintf(stderr,
-					"Error: Value out of range for a long integer\n");
-
-			} else if (val < INT_MIN || val > INT_MAX) {
-				fprintf(stderr,
-					"Error: Value out of range for a standard integer\n");
-
-			} else {
-				explain_code = (int)val;
-				only_explain = true;
-				show_string = true;
-			}
-		}
-	}
-
-
-	/* Program execution flags */
-	if (argc >= 3) {
-
-		/* Print return code */
-		if (strcmp(argv[2], "-e") == 0) {
-			flag_explain = true;
+		if (value == endptr) {
+			fprintf(stderr,
+				"error: no digits found after -e=\n");
+			return 1;
 		}
 
-		/* Explain return code with string */
-		else if (strcmp(argv[2], "-es") == 0) {
-			show_string = true;
+		if (*endptr != '\0') {
+			fprintf(stderr,
+				"error: invalid characters after return code: '%s'\n",
+				endptr);
+			return 1;
 		}
+
+		if (errno == ERANGE || val < INT_MIN || val > INT_MAX) {
+			fprintf(stderr,
+				"error: return code is out of range\n");
+			return 1;
+		}
+
+		explain_code = (int)val;
+		only_explain = true;
+		show_string = true;
 	}
 
 
 	/* =====================================================================
-	 * Standalone Return-Code Explanation
+	 * Standalone Explanation
 	 * ===================================================================== */
 	if (only_explain) {
 		explain_return_code(explain_code, show_string);
 		return 0;
+	}
+
+
+	/* =====================================================================
+	 * Program Execution Flags
+	 * ===================================================================== */
+
+	if (argc >= 3) {
+		if (strcmp(argv[2], "-e") == 0) {
+			flag_explain = true;
+		}
+		else if (strcmp(argv[2], "-es") == 0) {
+			flag_explain = true;
+			show_string = true;
+		}
+		else {
+			fprintf(stderr,
+				"error: unknown option '%s'\n",
+				argv[2]);
+			return 1;
+		}
+	}
+
+	/* Reject extra arguments */
+	if (argc > 3) {
+		fprintf(stderr,
+			"error: too many arguments\n"
+			"usage: lvm <program path> [option]\n");
+		return 1;
 	}
 
 
@@ -150,6 +175,7 @@ int main(int argc, char** argv)
 	if (!load_lbf_file(&file, argv[1])) {
 		goto cleanup;
 	}
+
 
 	/* Setup VM instance */
 	if (!setup_instance(&instance, &file)) {
@@ -173,9 +199,11 @@ int main(int argc, char** argv)
 	/* =====================================================================
 	 * Cleanup
 	 * ===================================================================== */
-	cleanup: 
+
+cleanup:
 	destroy_instance(&instance);
 	free_lbf_file(&file);
 
 	return 0;
 }
+
